cafcff1f5f308f44a15af15a60e7b5425d725ff4
vijaysakhare709 <129182134+vijaysakhare709@users.noreply.github.com>
2023-03-31 20:46:44 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,39 @@
+variable "resource-group-name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "storage-account-name" {
+ type = string
+}
+
+variable "storage-account-kind" {
+  type = string
+}
+
+variable "storage-account-tier" {
+    type = string
+}
+
+variable "storage-account-replication-type" {
+   type = string
+}
+
+variable "storage-blob-name" {
+    type = string
+}
+
+variable "storage-container-name" {
+   type = string
+}
+
+variable  "storage-blob-type" {
+    type = string
+}
+
+variable "storage-blob-content-type" {
+    type = string
+}
